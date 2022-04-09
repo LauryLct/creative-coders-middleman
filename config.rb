415181209628078a7a4ec46a71af6d9cf325c273
laury <laury.lecaut@gmail.com>
2022-04-09 18:29:51 +0200

    add legal mentions

--- config.rb
+++ config.rb
@@ -8,6 +8,8 @@ end
 # Layouts
 # https://middlemanapp.com/basics/layouts/
 
+page '/legal_mentions', layout: :layout
+
 # Per-page layout changes
 page '/*.xml', layout: false
 page '/*.json', layout: false
